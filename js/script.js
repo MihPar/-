357c367c7826aa13b4d0ca1c030140cbb00eb3c2
Mihail <90350801+MihPar@users.noreply.github.com>
2023-02-16 16:30:46 +0300

    10_commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
+function clock() {
+    const div = document.querySelector('div')
+    div.style.fontSize = '24px'
+    div.style.color = "yellow"
+    div.style.marginLeft = '150px'
+    let now = new Date()
+    div.innerText = now.toLocaleTimeString()
+}
+clock()
+setInterval(clock, 1000)
+
 let btn = document.querySelector('.btn')
 btn.onclick = function() {
     document.querySelector('.hidden').classList.remove('response')
